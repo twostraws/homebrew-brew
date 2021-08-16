5a5554636497e59192c888ca0506dcf920e9253a
Paul Hudson <paul@hackingwithswift.com>
2021-08-16 11:12:57 +0100

    Sitrep 2.0.0, for Swift 5.4.

--- sitrep.rb
+++ sitrep.rb
@@ -1,8 +1,8 @@
 class Sitrep < Formula
   desc "Sitrep is a source code analyzer for Swift projects, giving you a high-level overview of your code."
   homepage "https://github.com/twostraws/Sitrep"
-  url "https://github.com/twostraws/Sitrep/archive/1.0.0.zip"
-  sha256 "6316c73bb8bf10a77cd21d026f7939ce6efeea379b99df60de7ce0ff05913c71"
+  url "https://github.com/twostraws/Sitrep/archive/2.0.0.zip"
+  sha256 "e7da4676d65071ac7250e3fb9a7a6b544456396828adb43a1749616238cf7335"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   def install
