feef81701d2b26d6e69464dad0fe3ca6f9436018
Paul Hudson <paul@hackingwithswift.com>
2021-10-07 11:39:12 +0100

    Update for Xcode 13

--- sitrep.rb
+++ sitrep.rb
@@ -1,8 +1,8 @@
 class Sitrep < Formula
   desc "Sitrep is a source code analyzer for Swift projects, giving you a high-level overview of your code."
   homepage "https://github.com/twostraws/Sitrep"
-  url "https://github.com/twostraws/Sitrep/archive/2.0.0.zip"
-  sha256 "e7da4676d65071ac7250e3fb9a7a6b544456396828adb43a1749616238cf7335"
+  url "https://github.com/twostraws/Sitrep/archive/2.0.1.zip"
+  sha256 "a53d5b8a85ec862ada91f0b730cef71d748f61e7a91e26894366fcdeccc0c5ee"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   def install
